8cf1ffea11a28f0038fd1f02715fec26b5e65ddd
Draculoaf <2327163@students.wits.ac.za>
2022-06-07 15:47:01 +0200

    blog pages, cards and basic design outlines

--- blog.js
+++ blog.js
@@ -1,6 +1,6 @@
-function goBack() {
+/*function goBack() {
   window.history.back();
-}
+}*/
 
 function getArticle() {
   //Get elements
@@ -13,6 +13,9 @@ function getArticle() {
   sources[0] = "The Medium is the Message";
   sources[1] = "URLs";
   sources[2] = "Metadata";
+  sources[3] = "Information Geographies";
+  sources[4] = "Reflection on UX/UI";
+  sources[5] = "Internet, Soceity and Design Justice";
 
   //Random number
   let i = Math.floor(Math.random() * sources.length);
