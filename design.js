3326ba489e6e13d6480ae0e23e2659f8a6b54acc
Draculoaf <2327163@students.wits.ac.za>
2022-06-08 16:42:34 +0200

    UXUI blog post

--- design.js
+++ design.js
@@ -1,7 +1,3 @@
-//Get the button
-let myButton = document.getElementById("topBtn");
-
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
