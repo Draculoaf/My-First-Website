da71f36db9bf8b631103778b3a576c2bdb643672
Draculoaf <2327163@students.wits.ac.za>
2022-06-08 11:01:44 +0200

    Wireframes

--- design.js
+++ design.js
@@ -1,18 +1,5 @@
 //Get the button
-let myButton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    myButton.style.display = "block";
-  } else {
-    myButton.style.display = "none";
-  }
-}
+let myButton = document.getElementById("topBtn");
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
